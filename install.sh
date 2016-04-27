9df9836e7d140354b5f55f0484cfb38b881e8d72
Sagar Pandya <sagargp@gmail.com>
2016-04-27 14:17:34 -0700

    Fixed a typo in the install script

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ if hash stow 2>/dev/null; then
   stow --verbose=3 -t $HOME bash bin git tmux vim
   stow --verbose=3 -t $HOME/.config config
 else
-  echo "Please install `stow` and try again"
+  echo "Please install stow and try again"
   exit 1
 fi
